f3593c79ce146262be150d64df5a49847f3567df
Shravan Ryakam <shravanryakam1408@gmail.com>
2023-08-09 12:47:15 +0000

    Commit by shravan

---
+++ employeedetails/webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["com/employeedetails/employeedetails/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ employeedetails/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"comemployeedetails/employeedetails/test/unit/controller/View1.controller"
+], function () {
+	"use strict";
+});
